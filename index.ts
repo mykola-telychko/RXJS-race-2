237c2000162c494efb516e5bd000d52e9f0ad5ef
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-10 07:54:12 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,14 +1,26 @@
 import { delay, map } from 'rxjs/operators';
 import { of, race } from 'rxjs';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/race
+
 //Throws an error and ignores the other observables.
-const first = of('first').pipe(
-  delay(100),
+const first = of('first').pipe(delay(1000));
+const second = of('second').pipe(delay(200));
+const third = of('third').pipe(delay(300));
+const error = of('error-stop here(where) is the smallest delay').pipe(
+  delay(100)
   map(_ => {
     throw 'error';
   })
 );
-const second = of('second').pipe(delay(200));
-const third = of('third').pipe(delay(300));
+const fourth = of('third').pipe(delay(50));
+
+//  if observables have is the smallest delay 
+//  this block of code stops
+// map(_ => {
+//   throw 'error';
+// })
+
+
 // nothing logged
-race(first, second, third).subscribe(val => console.log(val));
+race(first, second, third, error, fourth).subscribe((val) => console.log(val));
